78a01d091b508b8a20b84bca2e0ef8d6e58969e2
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-08-25 13:05:47 +0600

    footer responsiveness updated

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -11,35 +11,37 @@ const Footer = () => {
         <div className='pt-20'>
             <div className='bg-[#00396A] h-8'></div>
             <div className='bg-gradient-to-tr from-[#004987] via-[#2F97BA] to-[#004987] text-white pt-10 pb-3'>
-                <div className='w-[93%] lg:w-[85%] mx-auto flex justify-between items-center'>
-                    <div className='w-full lg:w-1/4'>
+                <div className='w-[93%] lg:w-[85%] mx-auto flex flex-col lg:flex-row justify-between items-center space-y-8 md:space-y-0'>
+                    <div className='w-full lg:w-[50%]'>
                         <p className='text-3xl font-bold'>
                             NOIDERMA
                         </p>
-                        <p>
+                        <p className='lg:pr-36 text-justify'>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam minus non praesentium rerum soluta facilis tenetur labore perspiciatis, earum nostrum!
                         </p>
                     </div>
-                    <div className='w-full lg:w-1/4'>
-                        <p className='text-xl font-bold'>
-                            Office
-                        </p>
-                        <p className='address'>
-                            59,60 North Rajashon, <br />
-                            Birulia Road, Savar, <br />
-                            Dhaka-1340, Bangladesh.
-                        </p>
-                    </div>
-                    <div className='w-full lg:w-1/4'>
-                        <p className='text-xl font-bold'>
-                            Quick Links
-                        </p>
-                        <div className='flex flex-col'>
-                            <Link className='hover:link'>Products</Link>
-                            <Link className='hover:link'>Contacts</Link>
+                    <div className='flex justify-between items-center w-full lg:w-[30%]'>
+                        <div className='w-full lg:w-1/2'>
+                            <p className='text-xl font-bold'>
+                                Office
+                            </p>
+                            <p className='address'>
+                                59,60 North Rajashon, <br />
+                                Birulia Road, Savar, <br />
+                                Dhaka-1340, Bangladesh.
+                            </p>
+                        </div>
+                        <div className='w-full lg:w-1/2 pl-10'>
+                            <p className='text-xl font-bold'>
+                                Quick Links
+                            </p>
+                            <div className='flex flex-col'>
+                                <Link className='hover:link'>Products</Link>
+                                <Link className='hover:link'>Contacts</Link>
+                            </div>
                         </div>
                     </div>
-                    <div className='w-full lg:w-1/4'>
+                    <div className='w-full lg:w-[20%] flex flex-col justify-center items-center lg:items-start lg:pl-20'>
                         <p className='text-xl font-bold'>
                             Social Media
                         </p>
