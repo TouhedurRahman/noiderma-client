babfc63e88fec80b06ff8ed79c6061e43fb0fb7b
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-08-28 17:02:01 +0600

    categorized feature updated

--- src/Pages/Home/CategorizedFeature/CategorizedFeature.jsx
+++ src/Pages/Home/CategorizedFeature/CategorizedFeature.jsx
@@ -34,21 +34,21 @@ const CategorizedFeature = () => {
 
     return (
         <div className='w-[93%] md:w-[85%] mx-auto pt-20'>
-            <div>
+            <div className='grid grid-cols-1 gap-5'>
                 {
                     contents.map(content => (
                         <>
                             <div className={`m-5 my-10 flex flex-col ${(content?.flexprop) === 'reverse' ? 'md:flex-row-reverse' : 'md:flex-row'} lg:justify-between items-center rounded-lg`}>
-                                <div className="w-full md:w-1/2 lg:rounded-lg">
+                                <div className="w-full md:w-1/2 lg:rounded-lg flex justify-center items-center">
                                     <img
                                         src={content.image}
-                                        className="w-full h-full rounded-lg"
+                                        className="w-[417px] h-[501px] rounded-[25px]"
                                         alt="Loading..."
                                     />
                                 </div>
                                 <div className={`w-full space-y-8 md:w-1/2 pt-5 md:pt-0 ${(content?.flexprop) === 'reverse' ? 'md:me-20' : 'md:ms-20'} flex flex-col justify-center items-start`}>
                                     <p
-                                        className="mb-3 font-bold text-3xl text-[#004987]"
+                                        className="mb-3 font-bold text-3xl"
                                         style={{ letterSpacing: '0.2em' }}
                                     >
                                         {content.title.toUpperCase()}
@@ -56,11 +56,14 @@ const CategorizedFeature = () => {
                                     <p
                                         className="mb-3 text-justify text-[#5E656B] text-sm leading-relaxed"
                                     >
-                                        {content.description}
+                                        {/* {content.description} */}
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde libero eius repudiandae iure ad quos veritatis pariatur illo porro fuga culpa, enim blanditiis deleniti animi reiciendis nemo harum, in voluptatibus, ratione est nam cum! Dicta ipsam repellat ullam deserunt unde minus, voluptatum, neque ex itaque laudantium molestiae provident quaerat eligendi. <br /><br />
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sequi reprehenderit ratione facilis quam officiis consectetur non repudiandae perspiciatis odit.<br /><br />
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta nemo ipsam placeat atque assumenda exercitationem sed necessitatibus laudantium quidem enim, ea quos dignissimos ad quasi. Dolores ratione ipsam veritatis corrupti!
                                     </p>
                                     <Link
                                         to={content.link}
-                                        className='link text-[#38AACF]'
+                                        className='link'
                                     >
                                         {content.gototext}
                                     </Link>
