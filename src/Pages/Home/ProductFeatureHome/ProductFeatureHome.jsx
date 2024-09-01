a0162e92dec84d18d6f68f470ab233ba6d78671f
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-09-01 12:35:08 +0600

    product feature home updated

--- src/Pages/Home/ProductFeatureHome/ProductFeatureHome.jsx
+++ src/Pages/Home/ProductFeatureHome/ProductFeatureHome.jsx
@@ -4,33 +4,34 @@ import '@smastrom/react-rating/style.css';
 
 const ProductFeatureHome = () => {
     return (
-        <div>
-            <div className='mt-20'>
-                <div className='relative flex items-center justify-end'>
-                    <div className='absolute left-0 flex justify-start items-center ms-24 z-20'>
-                        <img
-                            src="https://via.placeholder.com/150?text=Dermatologist+Recommended"
-                            alt="Recommended"
-                            className='w-[600px] h-[350px] rounded-[25px]'
-                        />
-                    </div>
-                    <div className='bg-gradient-to-tr from-[#f5f5f5] to-[#e0e0e0] w-[75%] rounded-l-[25px] p-40 pe-20 space-y-10 z-10 text-right'>
-                        <p
-                            className='text-black text-4xl font-bold pl-52'
-                            style={{ letterSpacing: '0.2em' }}
-                        >
-                            HEALING OINTMENT FOR DRY, CRACKED SKIN
-                        </p>
-                        <p className='text-black pl-48'>
-                            Noiderma Healing Ointment is specially formulated with a high concentration of the active ingredient, petrolatum, to quickly protect and heal dry, irritated, sensitive skin.
-                        </p>
-                        <Link
-                            className="btn py-3 px-10 bg-white hover:bg-black border-2 border-[black] text-black hover:text-white rounded-full"
-                            style={{ letterSpacing: "0.1em" }}
-                        >
-                            MORE ON DRY SCREEN
-                        </Link>
-                    </div>
+        <div className='mt-20'>
+            <div className='relative flex flex-col lg:flex-row items-center justify-end'>
+                {/* Image Section */}
+                <div className='lg:absolute left-0 flex justify-center lg:justify-start items-center lg:ms-24 z-20'>
+                    <img
+                        src="https://via.placeholder.com/150?text=Dermatologist+Recommended"
+                        alt="Recommended"
+                        className='w-[300px] h-[175px] md:w-[450px] md:h-[250px] lg:w-[600px] lg:h-[350px] rounded-[25px]'
+                    />
+                </div>
+
+                {/* Text Section */}
+                <div className='bg-gradient-to-tr from-[#f5f5f5] to-[#e0e0e0] w-full lg:w-[75%] lg:rounded-l-[25px] p-8 md:p-20 lg:p-40 lg:pe-20 space-y-4 md:space-y-10 z-10 text-center lg:text-right mt-8 lg:mt-0'>
+                    <p
+                        className='text-black text-2xl md:text-3xl lg:text-4xl font-bold lg:pl-52'
+                        style={{ letterSpacing: '0.2em' }}
+                    >
+                        HEALING OINTMENT FOR DRY, CRACKED SKIN
+                    </p>
+                    <p className='text-black lg:pl-48'>
+                        Noiderma Healing Ointment is specially formulated with a high concentration of the active ingredient, petrolatum, to quickly protect and heal dry, irritated, sensitive skin.
+                    </p>
+                    <Link
+                        className="btn py-2 px-6 md:py-3 md:px-10 bg-white hover:bg-black border-2 border-[black] text-black hover:text-white rounded-full"
+                        style={{ letterSpacing: "0.1em" }}
+                    >
+                        MORE ON DRY SCREEN
+                    </Link>
                 </div>
             </div>
         </div>
