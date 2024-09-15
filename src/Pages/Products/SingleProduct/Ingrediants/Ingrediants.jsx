40fbf537c98373220c098832997ae65e638ac8f0
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-09-15 16:53:08 +0600

    ingredients functionality updated and responsiveness added

--- src/Pages/Products/SingleProduct/Ingrediants/Ingrediants.jsx
+++ src/Pages/Products/SingleProduct/Ingrediants/Ingrediants.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Ingrediants = ({ product }) => {
     return (
         <div className='mt-20 bg-[#E6E6E6] py-10'>
-            <div className='w-full md:w-[85%] mx-auto flex justify-between items-center'>
+            <div className='w-[93%] md:w-[85%] mx-auto flex flex-col md:flex-row justify-between items-center space-y-6'>
                 <div className='w-full md:w-1/2 flex flex-col justify-center items-start space-y-7'>
                     <p
                         className="mb-3 font-bold text-3xl"
@@ -50,6 +50,33 @@ const Ingrediants = ({ product }) => {
                         alt="Loading..."
                         className='w-full h-96 rounded-[25px]'
                     />
+                </div>
+            </div>
+            <div className='w-[93%] md:w-[85%] mx-auto mt-10 space-y-6'>
+                <p
+                    className="mb-3 font-bold text-3xl"
+                    style={{ letterSpacing: '0.2em' }}
+                >
+                    FIND OUT MORE ABOUT OUR INGREDIENTS
+                </p>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+                    {
+                        product.ingrediants.moreIngrediants.map(ingrediant => (
+                            <div className='flex flex-col space-y-4'>
+                                <img
+                                    src={ingrediant.moreIngrediantsImg}
+                                    alt="Loading..."
+                                    className='w-full h-72 rounded-[25px]'
+                                />
+                                <p className='text-xl font-bold'>
+                                    {ingrediant.title}
+                                </p>
+                                <p className='text-sm text-justify'>
+                                    {ingrediant.description}
+                                </p>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
