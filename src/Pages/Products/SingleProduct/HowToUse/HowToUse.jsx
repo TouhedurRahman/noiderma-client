a6ff7e6fbe4debd19980b69bf6d6025da7072e7a
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-09-15 11:57:23 +0600

    how to use section responsiveness added

--- src/Pages/Products/SingleProduct/HowToUse/HowToUse.jsx
+++ src/Pages/Products/SingleProduct/HowToUse/HowToUse.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const HowToUse = ({ product }) => {
     return (
         <div className='mt-20 bg-[#E6E6E6] py-10'>
-            <div className='w-full md:w-[85%] mx-auto flex justify-between items-center'>
+            <div className='w-[93%] md:w-[85%] mx-auto flex flex-col-reverse md:flex-row justify-between items-center'>
                 <div className='w-full md:w-1/2 flex flex-col justify-center items-start space-y-7'>
                     <p
-                        className="mb-3 font-bold text-3xl"
+                        className="mb-3 font-bold text-3xl mt-10 md:mt-0"
                         style={{ letterSpacing: '0.2em' }}
                     >
                         HOW TO USE
