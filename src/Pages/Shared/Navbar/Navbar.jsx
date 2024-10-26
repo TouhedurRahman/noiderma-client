4f19b3343d25cd2374a6eda0db9fdbf30debc620
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-10-26 15:32:28 +0600

    search functionality added in navbar

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FaBars, FaTimes, FaChevronDown, FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import useProducts from '../../../Hooks/useProducts';
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -8,6 +9,11 @@ const Navbar = () => {
     const [isSearchOpen, setIsSearchOpen] = useState(false);
     const [isScrolled, setIsScrolled] = useState(false);
     const [isHovered, setIsHovered] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredProducts, setFilteredProducts] = useState([]);
+
+    const [products, loading] = useProducts();
+    const navigate = useNavigate();
 
     useEffect(() => {
         const handleScroll = () => {
@@ -24,65 +30,53 @@ const Navbar = () => {
         setIsOpen(!isOpen);
     };
 
-    const toggleSubMenu = (index) => {
-        setActiveSubMenu(activeSubMenu === index ? null : index);
-    };
-
     const toggleSearch = () => {
         setIsSearchOpen(!isSearchOpen);
+        setSearchTerm(''); // Reset search term when toggling
+        setFilteredProducts([]);
+    };
+
+    const handleSearch = (e) => {
+        const query = e.target.value.toLowerCase();
+        setSearchTerm(query);
+
+        if (query) {
+            const results = products.filter(product =>
+                product.name.toLowerCase().includes(query)
+            );
+            setFilteredProducts(results);
+        } else {
+            setFilteredProducts([]);
+        }
+    };
+
+    const handleProductClick = (productId) => {
+        navigate(`/products/${productId}`);
+        toggleSearch();
     };
 
     const menuItems = [
-        // { name: 'PRODUCTS', subOptions: ['Product 1', 'Product 2'] },
         { name: 'PRODUCTS', subOptions: [] },
         { name: 'CONTACT US', subOptions: [] },
         { name: 'FIND A STORE', subOptions: [] }
     ];
 
     return (
-        <nav
-            className={`w-full fixed top-0 left-0 right-0 font-bold transition-colors duration-300 z-50 ${isScrolled || isHovered ? 'bg-gradient-to-tr from-[#f5f5f5] to-[#e0e0e0] text-black shadow-lg' : 'bg-transparent text-white'}`}
+        <nav className={`w-full fixed top-0 left-0 right-0 font-bold transition-colors duration-300 z-50 ${isScrolled || isHovered ? 'bg-gradient-to-tr from-[#f5f5f5] to-[#e0e0e0] text-black shadow-lg' : 'bg-transparent text-white'}`}
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
             <div className='w-[93%] lg:w-[85%] mx-auto flex justify-between items-center py-4'>
-                {/* Logo */}
                 <Link to="/" className='text-2xl font-bold flex justify-center items-center'>
-                    <img
-                        src={
-                            isScrolled || isHovered
-                                ? "https://i.ibb.co/Zm5bGzp/noiderma-black-logo.png"
-                                : "https://i.ibb.co/8Nmk5NC/noiderma-white-logo.png"
-                        }
-                        alt="Logo"
-                        className='w-[150px] lg:w-[180px] h-[40px]'
-                    />
+                    <img src={isScrolled || isHovered ? "https://i.ibb.co/Zm5bGzp/noiderma-black-logo.png" : "https://i.ibb.co/8Nmk5NC/noiderma-white-logo.png"} alt="Logo" className='w-[150px] lg:w-[180px] h-[40px]' />
                 </Link>
 
-                {/* Menu Items */}
                 <ul className='hidden md:flex space-x-4 ml-auto'>
                     {menuItems.map((item, index) => (
                         <li key={index} className='relative group'>
-                            <a
-                                href={`/${item.name.toLowerCase().replace(' ', '-')}`}
-                                className='cursor-pointer transition duration-300 hover:text-[#01305A]'
-                            >
+                            <a href={`/${item.name.toLowerCase().replace(' ', '-')}`} className='cursor-pointer transition duration-300 hover:text-[#01305A]'>
                                 {item.name}
                             </a>
-                            {item.subOptions.length > 0 && (
-                                <ul className='absolute left-0 mt-2 bg-[#FFFFFF] text-left py-2 space-y-1 rounded shadow-lg opacity-0 group-hover:opacity-100 transition duration-300 z-50'>
-                                    {item.subOptions.map((subOption, subIndex) => (
-                                        <li key={subIndex}>
-                                            <a
-                                                href={`/${item.name.toLowerCase().replace(' ', '-')}/${subOption.toLowerCase().replace(' ', '-')}`}
-                                                className='block px-4 py-2 hover:bg-[#014B88] hover:text-[#FFFFFF] transition duration-300'
-                                            >
-                                                {subOption}
-                                            </a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
                         </li>
                     ))}
                 </ul>
@@ -90,16 +84,14 @@ const Navbar = () => {
                 {/* Search Icon and Input */}
                 <div className='relative flex items-center ml-4'>
                     <div className="flex items-center transition-all duration-500 ease-in-out">
-                        <button
-                            onClick={toggleSearch}
-                            className={`text-xl transition-opacity duration-300 ${isSearchOpen ? 'opacity-0' : 'opacity-100'}`}
-                            style={{ pointerEvents: isSearchOpen ? 'none' : 'auto' }}
-                        >
+                        <button onClick={toggleSearch} className={`text-xl transition-opacity duration-300 ${isSearchOpen ? 'opacity-0' : 'opacity-100'}`} style={{ pointerEvents: isSearchOpen ? 'none' : 'auto' }}>
                             <FaSearch />
                         </button>
                         <input
                             type="text"
                             placeholder="Search..."
+                            value={searchTerm}
+                            onChange={handleSearch}
                             className={`border-2 border-gray-400 text-black px-4 py-2 rounded-lg focus:outline-none ml-2 transition-all duration-500 ease-in-out ${isSearchOpen ? 'w-[150px] opacity-100' : 'w-0 opacity-0'} focus:shadow-lg shadow-gray-900`}
                             style={{ visibility: isSearchOpen ? 'visible' : 'hidden' }}
                         />
@@ -109,6 +101,19 @@ const Navbar = () => {
                             </button>
                         )}
                     </div>
+                    {isSearchOpen && filteredProducts.length > 0 && (
+                        <div className="absolute top-[60px] left-0 w-[200px] bg-white border border-gray-200 rounded-lg shadow-lg z-50">
+                            {filteredProducts.map((product) => (
+                                <div
+                                    key={product._id}
+                                    onClick={() => handleProductClick(product._id)}
+                                    className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                                >
+                                    {product.name}
+                                </div>
+                            ))}
+                        </div>
+                    )}
                 </div>
 
                 {/* Mobile Menu Toggle */}
