667cdafc887f67ea231df273d2e6fddb7955ff29
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2024-11-03 16:31:50 +0600

    contact address updated

--- src/Pages/ContactUs/ContactAddress/ContactAddress.jsx
+++ src/Pages/ContactUs/ContactAddress/ContactAddress.jsx
@@ -3,8 +3,18 @@ import { FaBuilding, FaIndustry } from 'react-icons/fa';
 
 const ContactAddress = () => {
     return (
-        <div className="mt-20 p-8 bg-white">
-            <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="mt-20 py-8 bg-white">
+            <div className='w-[90%] mx-auto'>
+                <h2 className="text-3xl font-bold text-gray-600 mb-4">Manufacturing Plant</h2>
+                <p className="text-gray-800 mb-2 font-medium">SKY RESOURCES SDN. BHD.</p>
+                <address className="text-gray-600">
+                    No.1720, Lorong Perusahaan Utama 1, Kawasan Perindustrian,
+                    Bukit Tengah, 14000 Bukit Mertajam (SPT) Penang, Malaysia
+                </address>
+                <p className="text-gray-600">Phone: <span className="font-bold">+880 1329-747657</span></p>
+                <p className="text-gray-600">Email: <a href="mailto:info@navantispharma.com" className="text-gray-500 underline hover:text-gray-700">info@navantispharma.com</a></p>
+            </div>
+            {/* <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
                 <div className="relative p-6 bg-gradient-to-r from-gray-100 to-white shadow-lg rounded-lg hover:shadow-xl transition-shadow duration-300 text-center">
                     <FaBuilding className="absolute top-3 right-3 text-gray-500 w-10 h-10" />
 
@@ -28,7 +38,7 @@ const ContactAddress = () => {
                     <p className="text-gray-600">Phone: <span className="font-bold">+880 1329-747657</span></p>
                     <p className="text-gray-600">Email: <a href="mailto:info@navantispharma.com" className="text-gray-500 underline hover:text-gray-700">info@navantispharma.com</a></p>
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
