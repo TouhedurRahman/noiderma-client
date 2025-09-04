0ceb8ec49a3e11b6379cfc099d39077ceab6455b
TouhedurRahman <86222384+TouhedurRahman@users.noreply.github.com>
2025-09-04 14:43:09 +0600

    contact us page updated

--- src/Pages/ContactUs/ContactAddress/ContactAddress.jsx
+++ src/Pages/ContactUs/ContactAddress/ContactAddress.jsx
@@ -1,41 +1,30 @@
+import { FaPhone } from "react-icons/fa6";
+
 const ContactAddress = () => {
     return (
-        <div className="mt-20 py-8 bg-white">
-            <div className='w-[90%] mx-auto'>
+        <div className="mt-20 py-8 bg-white flex flex-col md:flex-row md:justify-between md:items-center space-y-8 md:space-y-0 w-[90%] mx-auto">
+            <div>
                 <h2 className="text-3xl font-bold text-gray-600 mb-4">Manufactured by</h2>
                 <p className="text-gray-800 mb-2 font-medium">SKY RESOURCES SDN. BHD.</p>
                 <address className="text-gray-600">
                     No.1720, Lorong Perusahaan Utama 1, Kawasan Perindustrian<br />
                     Bukit Tengah, 14000 Bukit Mertajam (SPT) Penang, Malaysia
                 </address>
-                {/* <p className="text-gray-600">Phone: <span className="font-bold">+880 1329-747657</span></p>
-                <p className="text-gray-600">Email: <a href="mailto:info@navantispharma.com" className="text-gray-500 underline hover:text-gray-700">info@navantispharma.com</a></p> */}
+
             </div>
-            {/* <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
-                <div className="relative p-6 bg-gradient-to-r from-gray-100 to-white shadow-lg rounded-lg hover:shadow-xl transition-shadow duration-300 text-center">
-                    <FaBuilding className="absolute top-3 right-3 text-gray-500 w-10 h-10" />
 
-                    <h2 className="text-3xl font-bold text-gray-600 mb-4">Head Office</h2>
-                    <p className="text-gray-800 mb-2 font-medium">Navantis Pharma Limited</p>
-                    <p className="text-gray-600">Mirpur - 1</p>
-                    <p className="text-gray-600">Dhaka, Bangladesh</p>
-                    <p className="text-gray-600">Phone: <span className="font-bold">+880 1329-747657</span></p>
-                    <p className="text-gray-600">Email: <a href="mailto:info@navantispharma.com" className="text-gray-500 underline hover:text-gray-700">info@navantispharma.com</a></p>
-                </div>
-
-                <div className="relative p-6 bg-gradient-to-r from-white to-gray-100 shadow-lg rounded-lg hover:shadow-xl transition-shadow duration-300 text-center">
-                    <FaIndustry className="absolute top-3 right-3 text-gray-500 w-10 h-10" />
-
-                    <h2 className="text-3xl font-bold text-gray-600 mb-4">Manufacturing Plant</h2>
-                    <p className="text-gray-800 mb-2 font-medium">SKY RESOURCES SDN. BHD.</p>
-                    <address className="text-gray-600">
-                        No.1720, Lorong Perusahaan Utama 1, Kawasan Perindustrian,
-                        Bukit Tengah, 14000 Bukit Mertajam (SPT) Penang, Malaysia
-                    </address>
-                    <p className="text-gray-600">Phone: <span className="font-bold">+880 1329-747657</span></p>
-                    <p className="text-gray-600">Email: <a href="mailto:info@navantispharma.com" className="text-gray-500 underline hover:text-gray-700">info@navantispharma.com</a></p>
-                </div>
-            </div> */}
+            <div>
+                <h2 className="text-3xl font-bold text-gray-600 mb-4">Marketed by</h2>
+                <p className="text-gray-800 mb-2 font-medium">NAVANTIS PHARMA LIMITED</p>
+                <address className="text-gray-600">
+                    59,60 North Rajashon,
+                    Birulia Road, Savar,
+                    Dhaka-1340, Bangladesh.
+                </address>
+                <p className="flex justify-start items-center">
+                    <FaPhone className="me-2" /> +880 1329-747657
+                </p>
+            </div>
         </div>
     );
 };
